6672c85125156e437f7d50bcd3d70e124e279454
Michel-Motato <michelmotato11@gmail.com>
2020-08-12 17:26:26 -0500

    Creacion del proyecto

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+
+function App() {
+  return (
+    <div className="App">
+              <tr>
+                <th> Nombres</th>
+                <th> Apellidos </th>
+                <th> Correo</th>
+                <th> telefono </th>
+              </tr>
+            
+              <tr>
+                <td> Michel </td>
+                <td> Motato </td>
+                <td> michelmotato11@gmail.com</td>
+                <td> 3184428767</td>
+              </tr>
+
+              <tr>
+                <td> Alisson</td>
+                <td> Rios</td>
+                <td> alisrios2511@gmail.com </td>
+                <td> 3006132278</td>
+              </tr>
+
+              <tr>
+                <td> Alexandra </td>
+                <td>Fernández </td>
+                <td> neybran2230@gmail.com</td>
+                <td>3044469316 </td>
+              </tr>
+    </div>
+  );
+}
+
+export default App;
