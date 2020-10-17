aaeefe4832a3d1ab5306c1f2ccab688153a6d304
Michel-Motato <michelmotato11@gmail.com>
2020-10-16 22:28:46 -0500

    Subiendo Proyecto

---
+++ src copy/Body.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import './App';
+
+
+ function Body(){
+
+    return(
+        <div className="App">
+               <h1>¿Què es?</h1>
+               <p>Los trastornos bipolares y otros trastornos relacionados son trastornos episódicos del estado de ánimo definidos por la presencia de episodios o síntomas maníacos, mixtos o hipomaníacos. Estos episodios generalmente se alternan a lo largo del curso de estos trastornos con episodios depresivos o períodos de síntomas depresivos.</p>
+
+<h1>Significado:</h1>
+
+<p><li><strong>Hipomaníacos:</strong> Es un afectivo caracterizado por un ánimo persistentemente expansivo, hiperactivo y/o irritable.</li></p>
+
+<p><li><strong>Maniacos:</strong> Es un periodo delimitado y persistente, durante el cual hay un estado de ánimo anormalmente elevado, expansivo o irritable, menos frecuente que el depresivo y relativamente fácil de detectar</li></p>
+
+<p><li><strong>Mixtos:</strong> Integrado por varios contextos. </li></p>
+
+<p><li><strong>Depresivos: </strong>Es un trastorno mental frecuente, que se caracteriza por la presencia de tristeza, pérdida de interés o placer, sentimientos de culpa o falta de autoestima,
+</li></p>
+        </div>)
+
+return (
+<div className="paginatios">
+<ul>
+<li><a href="#">&laquo;</a></li>
+  <li><a href="Body.jsx">1</a></li>
+  <li><a href="#">2</a></li>
+  <li><a href="#">3</a></li>
+  <li><a href="#">4</a></li>
+  <li><a href="#">5</a></li>
+  <li><a href="#">&raquo;</a></li>
+</ul>
+</div>)
+
+}
+    
+export default Body;
---
+++ src copy/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import index from './index';
+import Body from './Body';
+import Body2 from './Body2';
+import Body3 from './Body3';
+import Body4 from './Body4';
+import Body5 from './Body5';
+import Comentario from './Comentario';
+import Valoracion from './Valoracion';
+import Registrarte from './Registrate';
+import Usuario from './Usuario';
+import Menu from './Menu';
+import Chat from './Chat';
+
+
+
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import $ from 'jquery';
+	import Popper from 'popper.js';
+  import 'bootstrap/dist/js/bootstrap.bundle.min';
+import Registrate from './Registrate';
+  
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Menu/>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
